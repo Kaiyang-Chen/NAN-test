89c7e7c71013d3797b135ccc4336e9a4c92925aa
chenkaiyang <chenkaiyang@bytedance.com>
2020-12-26 21:21:56 +0800

    route error

--- generate_csv.py
+++ generate_csv.py
@@ -3,7 +3,7 @@ import os
 import csv
 import random
 
-datapath = '/home/lss/Desktop/cky/NAN_test/Conrad Dataset'
+datapath = '/home/lss/Desktop/cky/NAN-test/Conrad Dataset'
 file_haeader = ["subject","template","feature_path"]
 csvTrain = open("train.csv", "w")
 writer_train = csv.writer(csvTrain)
@@ -13,7 +13,8 @@ persons = os.listdir(datapath)
 writer_test.writerow(file_haeader)
 writer_train.writerow(file_haeader)
 for person in persons:
-    templates = os.listdir(person)
+    person_dir = os.path.join(datapath,person)
+    templates = os.listdir(person_dir)
     train_head = 0
     test_head = 0
     train_speak = 0
@@ -23,14 +24,16 @@ for person in persons:
         if (template[0:3] == "head"):
             if((random.random()<0.33 & test_head < 1) | train_head == 2):
                 test_head =  test_head + 1
-                frames = os.listdir(template)
+                template_dir = os.path.join(person_dir,template)
+                frames = os.listdir(template_dir)
                 for frame in frames:
                     feature_path = os.path.join(datapath,person,template,frame)
                     tmp = [person,template,feature_path]
                     writer_test.writerow(tmp)
             else:
                 train_head = train_head + 1
-                frames = os.listdir(template)
+                template_dir = os.path.join(person_dir,template)
+                frames = os.listdir(template_dir)
                 for frame in frames:
                     feature_path = os.path.join(datapath,person,template,frame)
                     tmp = [person,template,feature_path]
@@ -38,14 +41,16 @@ for person in persons:
         else:
             if((random.random()<0.3 & test_speak < 3) | train_speak == 7):
                 test_speak =  test_speak + 1
-                frames = os.listdir(template)
+                template_dir = os.path.join(person_dir,template)
+                frames = os.listdir(template_dir)
                 for frame in frames:
                     feature_path = os.path.join(datapath,person,template,frame)
                     tmp = [person,template,feature_path]
                     writer_test.writerow(tmp)
             else:
                 train_head = train_head + 1
-                frames = os.listdir(template)
+                template_dir = os.path.join(person_dir,template)
+                frames = os.listdir(template_dir)
                 for frame in frames:
                     feature_path = os.path.join(datapath,person,template,frame)
                     tmp = [person,template,feature_path]
