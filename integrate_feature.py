ebccd3015862ff4b321aefd44279361923563a55
chenkaiyang <chenkaiyang@bytedance.com>
2021-01-17 17:20:07 +0800

    add prepare_data.py

--- integrate_feature.py
+++ integrate_feature.py
@@ -1,6 +1,7 @@
 import numpy as np 
 import os
 import csv
+import scipy.io as sio
 
 datapath = '/home/lss/Desktop/cky/NAN-test/Conrad Dataset'
 persons = os.listdir(datapath)
@@ -12,18 +13,22 @@ for person in persons:
     for template in templates:
         
         feature_dir = os.path.join(person_feature,template)
-        
+        des_dir = os.path.join(feature_dir,template+".txt")
+        mat_dir = os.path.join(feature_dir,template+".mat")
         frames = os.listdir(feature_dir)
         for frame in frames:
             des = os.path.join(feature_dir,frame)
-            feature_path = os.path.join(template_dir,frame)
-            des_dir = os.path.join(template_dir,template+".txt")
-            des = open('des_dir','a')
+            feature_path = os.path.join(feature_dir,frame)
+            
+            des = open(des_dir,'a')
             with open(feature_path,'r') as filein:
                 line = filein.readline()
                 des.write(line)
             des.close()
-        
+        data = np.loadtxt(des_dir,skiprows=0)
+        mdic = {'feat':data}
+        sio.savemat(mat_dir,mdic)
+    
 
             
 
---
+++ prepare_data.py
@@ -0,0 +1,54 @@
+import os
+import scipy.io as sio
+import numpy as np
+import pickle
+
+def save(lst, path):
+    with open(path, 'wb') as fp:
+        pickle.dump(lst, fp)
+
+def get_mat(feature_dir):
+    arr = []
+    item = sio.loadmat(feature_dir)
+    arr.append(item['feat'])
+    return arr
+
+def get_train_test_set():
+    data_dir_base = './Conrad Dataset'
+    save_dir = './'
+    train_dir = './'
+    train_file = os.path.join(train_dir,'train.csv')
+    cur_line = 0
+    subs = []
+    templates = []
+    lst_train_faces = []
+    for line in open(train_file):
+        cur_line += 1
+        if(cur_line == 1):
+            continue
+        lst_tmp = line.splitre(',')
+        template_id = lst_tmp[1]
+        subject_id = lst_tmp[0]
+        fearture_dir = os.path.join(data_dir_base,subject_id,"feature",template_id,template_id,".mat")
+        template_id = subject_id + "/" + template_id
+
+        if templates.count(template_id) == 0:
+            templates.append(template_id)
+            if subs.count(subject_id) == 0:
+                subs.append(subject_id)
+                lst_train_faces.append([])
+            idx = subs.index(subject_id)
+
+            arr = get_mat(fearture_dir)
+            if len(lst_train_faces[idx]) == 0:
+                lst_train_faces[idx] = arr
+            else:
+                lst_train_faces[idx] = np.vstack([lst_train_faces[idx], arr])
+
+    save(lst_train_faces, os.path.join(save_dir, 'train_subject.bin'))
+    print('#train subject {}'.format(len(lst_train_faces)))
+
+
+
+if __name__ == '__main__':
+    get_train_test_set()
